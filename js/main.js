6c9baec151339f93528573ca95e1dc113e840c60
ngf93 <nabiullinaguzel93@gmail.com>
2021-03-19 14:40:50 +0300

    save 5

--- js/main.js
+++ js/main.js
@@ -256,3 +256,46 @@ var mySwiper5 = new Swiper('.gallery', {
         swiper: mySwiper4,
       },
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const createShareHtml = (title, shareData) => {
+    let anchorList = [];
+    const anchorTemplate = (item) => {
+      return `<a href="${item.href}" target="_blank" rel="noopener">${item.title}</a>`;
+    }
+    const wrapperTemplate = (title, anchorList) => {
+      return `<div class="social-share__title">${title}</div><div class="social-share__items">${anchorList.join('')}</div>`
+    }
+    shareData.forEach(item => {
+      anchorList.push(anchorTemplate(item));
+    })
+    return wrapperTemplate(title, anchorList);
+  }
+  const $socialShare = document.querySelector('.social-share');
+  if ($socialShare) {
+    const url = encodeURIComponent(location.protocol + '//' + location.host + location.pathname);
+    const title = encodeURIComponent(document.title);
+    const twitterUserName = 'itchief_ru';
+    const shareData = [
+      {
+        title: 'Twitter',
+        href: `https://twitter.com/intent/tweet?text=${title}&url=${url}&via=${twitterUserName}`,
+      },
+      {
+        title: 'Facebook',
+        href: `https://www.facebook.com/sharer/sharer.php?u=${url}`,
+      },
+      {
+        title: 'ВКонтакте',
+        href: `https://vk.com/share.php?url=${url}`,
+      },
+      {
+        title: 'Telegram',
+        href: `https://t.me/share/url?url=${url}&text=${title}`,
+      }
+    ];
+    const $html = createShareHtml('Поделиться этим материалом в социальных сетях:', shareData);
+    $socialShare.innerHTML = $html;
+  }
+});
